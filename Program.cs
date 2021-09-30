25f3a02c025674948dd35078654ba424736e9b08
Jonialtiu <71144402+Jonialtiu@users.noreply.github.com>
2021-10-01 00:47:20 +0500

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,68 +1,88 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System;
+using System.IO;
 
-namespace ConsoleApp1
+namespace CodeGray
 {
     class Program
     {
+        static bool[][] GraysCode(int n)
+        {
+            bool[][] num = new bool[(int)Math.Pow(2, n)][];
+            if (n == 1)
+            {
+                num[0] = new bool[1];
+                num[1] = new bool[1];
+                num[0][0] = false;
+                num[1][0] = true;
+            }
+            else
+            {
+                bool[][] oldCode = GraysCode(n - 1);
+                int length = (int)Math.Pow(2, n) / 2;
+                for (int i = 0; i < length; i++)
+                {
+                    num[i] = new bool[n];
+                    for (int j = 0; j < n - 1; j++)
+                        num[i][j] = oldCode[i][j];
+                    num[i][n - 1] = false;
+                }
+                Array.Reverse(oldCode);
+                for (int i = 0; i < length; i++)
+                {
+                    num[i + length] = new bool[n];
+                    for (int j = 0; j < n - 1; j++)
+                        num[i + length][j] = oldCode[i][j];
+                    num[i + length][n - 1] = true;
+                }
+            }
+            return num;
+        }
         static void Main(string[] args)
         {
-            int numb = 1;
-            int numb2 = 1;
-            //--- Ввод цисел ---
-            Console.Write("Сколько будет чисел в массиве?\nСтроки:  ");
-            numb2 = Convert.ToInt32(Console.ReadLine());
-            Console.Write("Столбцы: ");
-            numb = Convert.ToInt32(Console.ReadLine());
-            //--- Массив ---
-            Random r = new Random();
-            string y = "";
-            int[,] mas = new int[numb, numb2];
-            for (int i = 0; i < numb; i++)
+            string[] Arr;
+            int Size = 0;
+            bool done = false;
+            while (!done)
             {
-                for (int j = 0; j < numb2; j++)
-                {
-                    mas[i,j] = r.Next(0, 100); //--- Генерация цисел в массив ---
-                }
-            }
-            //--- Цикл ---
-            bool restart = false;
-            do
-            {
-                
-                for (int i = 0; i < numb; i++)
-                {
-                    for (int j = 0; j < numb2; j++)
-                        Console.Write(mas[i, j] + " "); //--- Вывод цесел массива ---
-                    Console.Write("\n");
-                }
-                Console.WriteLine("Для очистки массива, введите cancel");
-                y = Console.ReadLine();
-                if (y == "cancel")
-                {
-                    for (int i = 0; i < numb; i++)
-                    {
-                        for (int j = 0; j < numb2; j++)
-                        {
-                            mas[i, j] = 0;
-                            Console.Write(mas[i, j] + " "); //--- Очистка массива и вывод ---
-                        }
-                        Console.Write("\n");
-                    }
-                    restart = false;
-                }
+                Console.Write("Введите размер массива:");
+                if (int.TryParse(Console.ReadLine(), out Size) == false)
+                    Console.WriteLine("Можно ввести только число!");
                 else
-                {
-                    Console.WriteLine("      Error\n"); //--- Если "cancel" не был введён ---
-                    restart = true;
-                }
+                    if (Size < 1 || Size > 7)
+                        Console.WriteLine("Размер должен быть больше нуля и меньше 8!");
+                    else
+                        done = true;
             }
-            while (restart == true);
-            
-            Console.ReadKey();
+            Arr = new string[Size];
+            for (int i = 0; i < Size; i++)
+            {
+                Console.Write($"Введите элемент под номером {i + 1}:");
+                Arr[i] = Console.ReadLine();
+                if (Arr[i] == "" || Arr[i] == " ")
+                    Arr[i] = $"{(i + 1)}";
+            }
+            bool[][] grCode = GraysCode(Size);
+            int aSize = (int)Math.Pow(2, Size);
+            Char[] chr = { ' ', ',' };
+            Console.WriteLine("Код: ");
+            for (int i = 0; i < aSize; i++)
+            {
+                Console.Write($"Y{i + 1} => ");
+                for (int j = 0; j < Size; j++)
+                    if (grCode[i][j])
+                        Console.Write("1");
+                    else
+                        Console.Write("0");
+                Console.Write(" --> ");
+                string staple = "{";
+                for (int j = 0; j < Size; j++)
+                    if (grCode[i][j])
+                        staple += Arr[j].ToString() + ", ";
+                staple = staple.TrimEnd(chr);
+                staple += "}";
+                Console.Write(staple);
+                Console.WriteLine();
+            }
         }
     }
 }
